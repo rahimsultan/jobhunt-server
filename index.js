37de102918d662ac82dce576ef2fb7d187f2ffbc
rahimsultan <sulrahim3@gmail.com>
2023-11-05 17:05:24 +0600

    create specific job post api

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
+// const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
 
-
+// for body data parse in the post api formate
+app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+    }))
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@atlascluster.fvnorwx.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -41,10 +49,20 @@ async function run() {
     app.get('/api/jobs/category', async(req, res)=>{
         const category = req.query.category
         const query = {category : category}
-        console.log(category);
+        // console.log(category);
         const result = await jobsCollection.find(query).toArray();
         res.send(result)
     })
+
+    // get specific job post by id
+    app.get('/api/job/:jobid', async(req, res)=>{
+      const id = req.params.jobid;
+      console.log(id);
+      const query = {_id: new ObjectId(id)}
+      const result = await jobsCollection.findOne(query)
+      res.send(result)
+    })
+
 
 
 
